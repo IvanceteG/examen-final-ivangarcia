6ab708b3698d66ccfd7c139ef1c269dcfb9adde7
IvanceteG <garciacuestaivan@fpllefia.com>
2026-06-02 20:28:01 +0200

    Datos actualizados de nuevo, faltaban cosas

--- api/data/data.js
+++ api/data/data.js
@@ -1,8 +1,8 @@
 const events = [
   { id: "ev1", nom: "Hackathon Fullstack", data: "2026-06-20", ciutat: "Barcelona" },
-  { id: "ev2", nom: "React Summit",        data: "2026-07-15", ciutat: "Amsterdam"  },
-  { id: "ev3", nom: "Node Congress",       data: "2026-08-10", ciutat: "Madrid"     },
-  { id: "ev4", nom: "Next.js Conf",        data: "2026-09-05", ciutat: "San Francisco" },
+  { id: "ev2", nom: "Hackathon Fullstack", data: "2026-06-20", ciutat: "Barcelona" },
+  { id: "ev3", nom: "Hackathon Fullstack", data: "2026-06-20", ciutat: "Barcelona" },
+  { id: "ev4", nom: "Hackathon Fullstack", data: "2026-06-20", ciutat: "Barcelona" },
 ];
 
 module.exports = events;

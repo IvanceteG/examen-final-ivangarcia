a6fb6ad961bdcf0bbf291b398a3feddffdb30ba7
IvanceteG <garciacuestaivan@fpllefia.com>
2026-06-02 19:58:00 +0200

    Con link, vamos a la pagina events y ya se ven los eventos

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
-import EventCard from "./components/EventCard";
 
 export default function HomePage() {
     return (
         <main className="p-10">
             <h1 className="text-3xl font-bold">Gestor d’Esdeveniments</h1>
             <p className="text-gray-600 mt-2">Benvingut al projecte del mòdul.</p>
+            <Link href="/events" className="inline-block mt-4 px-4 py-2 bg-gray-600 text-white rounded hover:bg-gray-800">
+                Veure esdeveniments
+            </Link>
         </main>
-    )
+    );
 }
